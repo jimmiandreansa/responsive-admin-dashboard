f3b0800a1212c44a0ab286e06b23b7abb3865387
jimmiandreansa <jimmiandreansa11@gmail.com>
2022-10-17 13:35:20 +0700

    Responsive Admin Dashboard

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: "87490",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "Logitech Keyboard Master G2100",
+        productNumber: "19860",
+        paymentStatus: "Refunded",
+        shipping: "Declined"
+    },
+    {
+        productName: "Miniso Farfumed Lavender",
+        productNumber: "77890",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "AK2 Fold Grinder",
+        productNumber: "34562",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "MecaAir Keyboard Wireless",
+        productNumber: "19820",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "Standing Machine for Study",
+        productNumber: "65389",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "Yoga Slim 64",
+        productNumber: "13208",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    }
+]
